eb3073c8f8d7e11d4bace411d73527abb1353327
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-17 16:04:17 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
   i = 0 
   while i < collection.length 
   if yield(collection[i])
-    new_array.push(collection.i)
+    new_array.push(collection[i])
   i+=1
 end
 new_array
